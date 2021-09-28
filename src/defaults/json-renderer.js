ab2eb17ab8601df80f67c332aeca068bd58de5b2
kequc <v2xyvxkr.1646bpmi@getinbox.io>
2021-09-28 21:03:33 +0200

    Cleanup

--- src/defaults/json-renderer.js
+++ src/defaults/json-renderer.js
@@ -1,27 +1,27 @@
 const NODE_ENV = process.env.NODE_ENV || 'development';
 
 function jsonRenderer (payload, { method, res, errors }) {
-  try {
-    const json = generateJson(payload);
+  const json = generateJson(payload, errors);
 
-    res.setHeader('Content-Length', json.length);
+  res.setHeader('Content-Length', json.length);
 
-    if (method === 'HEAD') {
-      res.end();
-    } else {
-      res.end(json);
-    }
-  } catch (error) {
-    throw errors.InternalServerError('Invalid json response', { payload, error });
+  if (method === 'HEAD') {
+    res.end();
+  } else {
+    res.end(json);
   }
 }
 
 module.exports = jsonRenderer;
 
-function generateJson (payload) {
-  if (NODE_ENV === 'production') {
-    return JSON.stringify(payload);
+function generateJson (payload, errors) {
+  try {
+    if (NODE_ENV === 'production') {
+      return JSON.stringify(payload);
+    } else {
+      return JSON.stringify(payload, null, 2);
+    }
+  } catch (error) {
+    throw errors.InternalServerError('Invalid json response', { payload, error });
   }
-
-  return JSON.stringify(payload, null, 2);
 }
--- src/defaults/text-renderer.js
+++ src/defaults/text-renderer.js
@@ -1,21 +1,21 @@
 function textRenderer (payload, { method, res, errors }) {
-  try {
-    const text = generateText(payload);
+  const text = generateText(payload, errors);
 
-    res.setHeader('Content-Length', text.length);
+  res.setHeader('Content-Length', text.length);
 
-    if (method === 'HEAD') {
-      res.end();
-    } else {
-      res.end(text);
-    }
-  } catch (error) {
-    throw errors.InternalServerError('Invalid text response', { payload, error });
+  if (method === 'HEAD') {
+    res.end();
+  } else {
+    res.end(text);
   }
 }
 
 module.exports = textRenderer;
 
-function generateText (payload) {
-  return String(payload);
+function generateText (payload, errors) {
+  try {
+    return String(payload);
+  } catch (error) {
+    throw errors.InternalServerError('Invalid text response', { payload, error });
+  }
 }
