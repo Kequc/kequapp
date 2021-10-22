fcbbdfda02f9e8e5a1dc1781fb4d17b792851c9d
kequc <v2xyvxkr.1646bpmi@getinbox.io>
2021-10-22 19:50:24 +0200

    Formatting

--- src/defaults/error-handler.ts
+++ src/defaults/error-handler.ts
@@ -4,27 +4,18 @@ import { Bundle } from '../main';
 
 const NODE_ENV = process.env.NODE_ENV || 'development';
 
-
-type ErrorResult = {
-    error: {
-        statusCode: number,
-        message: string,
-        stack?: any,
-        info?: any
-    }
-};
-
-
 function errorHandler (error: ServerError, { res }: Bundle): BodyJson {
     const statusCode = error.statusCode || 500;
 
     res.statusCode = statusCode;
     res.setHeader('Content-Type', 'application/json; charset=utf-8');
 
-    const result: ErrorResult = {
+    const result = {
         error: {
             statusCode,
-            message: error.message
+            message: error.message,
+            stack: undefined as any,
+            info: undefined as any
         }
     };
 
