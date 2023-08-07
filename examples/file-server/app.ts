3cdacc2e192347ec8631c94e31738f5a7e300929
root <root@DESKTOP-AHNQ5S4.localdomain>
2023-08-07 20:28:32 +0300

    Fix regex

--- examples/file-server/app.ts
+++ examples/file-server/app.ts
@@ -1,11 +1,13 @@
 import { createApp, staticDirectory, staticFile } from '../../src/main'; // 'kequapp'
 
-const app = createApp().add(
-    staticDirectory('/assets/**', {
-        dir: '/examples/file-server/assets',
-        exclude: ['/examples/file-server/assets/private.txt']
-    }),
-    staticFile('/examples/file-server/assets/index.html')
-);
+const app = createApp({
+    routes: [
+        staticDirectory('/assets/**', {
+            dir: '/examples/file-server/assets',
+            exclude: ['/examples/file-server/assets/private.txt']
+        }),
+        staticFile('/examples/file-server/assets/index.html')
+    ]
+});
 
 export default app;
