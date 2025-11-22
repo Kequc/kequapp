9b2355279c7a82c9e6a70aec34aab4080a1f3c7d
Kequc <v2xyvxkr.1646bpmi@getinbox.io>
2025-11-22 10:53:57 +0100

    Cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
 export { sendFile } from './built-in/helpers/send-file.ts';
 export { staticDirectory } from './built-in/helpers/static-directory.ts';
-export { jsonRenderer } from './built-in/json-renderer.ts';
-export { textRenderer } from './built-in/text-renderer.ts';
 export { Ex, unknownToEx } from './built-in/tools/ex.ts';
 export { inject } from './built-in/tools/inject.ts';
 export * from './router/modules.ts';
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,13 +1,17 @@
 import assert from 'node:assert/strict';
 import { it } from 'node:test';
 import {
-    createAction,
     createApp,
     createBranch,
+    createRoute,
+    createAction,
     createErrorHandler,
     createRenderer,
-    createRoute,
     Ex,
+    unknownToEx,
+    inject,
+    FakeReq,
+    FakeRes,
     sendFile,
     staticDirectory,
 } from '../src/index.ts';
@@ -15,11 +19,15 @@ import {
 it('exports a lot of stuff', () => {
     assert.equal(typeof createApp, 'function');
     assert.equal(typeof createBranch, 'function');
+    assert.equal(typeof createRoute, 'function');
+    assert.equal(typeof createAction, 'function');
     assert.equal(typeof createErrorHandler, 'function');
     assert.equal(typeof createRenderer, 'function');
-    assert.equal(typeof createRoute, 'function');
+    assert.equal(typeof Ex, 'object');
+    assert.equal(typeof unknownToEx, 'function');
+    assert.equal(typeof inject, 'function');
+    assert.equal(typeof FakeReq, 'function');
+    assert.equal(typeof FakeRes, 'function');
     assert.equal(typeof sendFile, 'function');
     assert.equal(typeof staticDirectory, 'function');
-    assert.equal(typeof createAction, 'function');
-    assert.equal(typeof Ex, 'object');
 });
